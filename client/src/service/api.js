1bf8784bc396071f2df6d1df752318f6bf2c07d6
Ansari Irfan <115148633+Ansari-Irfan-360@users.noreply.github.com>
2024-02-27 15:07:25 +0530

    Update api.js

--- client/src/service/api.js
+++ client/src/service/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import { API_NOTIFICATION_MESSAGES, SERVICE_URLS } from '../constants/config';
 import { getAccessToken, getRefreshToken, setAccessToken, getType } from '../utils/common-utils';
 
-const API_URL = 'reflect-ibvy.onrender.com';
+const API_URL = 'http://reflect-ibvy.onrender.com';
 
 const axiosInstance = axios.create({
     baseURL: API_URL,
